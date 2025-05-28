de45c0724e91dc74a5f469719c04dd145e9df3ba
graciella-m <graciellamaiamachado@gmail.com>
2025-05-28 11:18:16 -0300

    Criação do projetoo e envio para IDE e início da estrutura do Package Controller

---
+++ src/main/java/br/graci/eti/Fastfurious/FastfuriousApplication.java
@@ -0,0 +1,13 @@
+package br.graci.eti.Fastfurious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfuriousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfuriousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/graci/eti/Fastfurious/FastfuriousApplicationTests.java
@@ -0,0 +1,13 @@
+package br.graci.eti.Fastfurious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfuriousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
